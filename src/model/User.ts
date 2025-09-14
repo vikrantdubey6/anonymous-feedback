3a9ce68b373d95b7301eca67871dca660dda0e45
vikrant dubey <Vikrant2003aug06@gmail.com>
2025-09-14 16:30:22 +0530

    setup

---
+++ src/model/User.ts
@@ -0,0 +1,83 @@
+import mongoose, {Schema, Document} from "mongoose";
+
+export interface Message extends Document{
+    content: string;
+    createdAt: Date
+}
+
+const MessageSChema: Schema<Message> = new Schema({
+
+    content:{
+        type: String,
+        required: true
+    },
+    createdAt:{
+        type: Date,
+        required: true,
+        default: Date.now
+    }
+
+})
+
+
+export interface User extends Document{
+    username: string;
+    email: string;
+    password:string;
+    verifyCode:string;
+    verifyCodeExpiry:Date;
+    isVerified:boolean;
+    isAcceptingMessage:boolean
+    messages: Message[]
+    createdAt: Date
+}
+
+const UserSchema: Schema<User> = new Schema({
+    username:{
+        type: String,
+        required: [true, "Username is required"],
+        trim: true,
+        unique:true
+    },
+    email:{
+        type: String,
+        required: [true, "Email is required"],
+        trim: true,
+        unique:true,
+          match: [/[a-z0-9\._%+!$&*=^|~#%'`?{}/\-]+@([a-z0-9\-]+\.){1,}([a-z]{2,16})/, "please use a valid email address"]
+    },
+    password:{
+        type: String,
+        required: [true, "Password is required"],
+        trim: true,
+        unique:true,
+      
+    },
+    verifyCode:{
+        type: String,
+        required: [true, "verify code is required"]
+    },
+    verifyCodeExpiry:{
+        type: Date,
+        required: [true, "verify code expiry is required"]
+    },
+    isVerified:{
+        type: Boolean,
+        default: false
+    },
+    isAcceptingMessage:{
+        type: Boolean,
+        default: true
+    },
+    messages:[MessageSChema],
+
+    createdAt:{
+        type: Date,
+        required: true,
+        default: Date.now
+    }
+})
+
+const UserModel = (mongoose.models.User as mongoose.Model<User>) || mongoose.model<User>("User", UserSchema)
+
+export default UserModel;
