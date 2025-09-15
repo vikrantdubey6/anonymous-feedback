4185fa67ad23ce9c1b92244f7f24d22e28be6b00
vikrant dubey <Vikrant2003aug06@gmail.com>
2025-09-15 07:19:32 +0530

    Standardization

---
+++ src/helpers/sendVerficationEmail.ts
@@ -0,0 +1,19 @@
+import { resend } from "@/lib/resend";
+import VerificationEmail from "../../emails/VerificationEmail";
+import { ApiResponse } from "@/types/ApiResponse";
+
+export async function sendVerificationEmail(
+
+    email: string,
+    username: string,
+    verifyCode: string
+
+): Promise<ApiResponse>{
+    try{
+        
+
+    }catch(error){
+
+
+    }
+}
