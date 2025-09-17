6b7b7ce4e9e5d674cd17e874ed324a87bfb334e2
vikrant dubey <Vikrant2003aug06@gmail.com>
2025-09-17 23:31:08 +0530

    auth

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,73 @@
+import {NextAuthOptions} from "next-auth";
+import CreadentialsProvider from "next-auth/providers/credentials"
+import bcrypt from "bcryptjs";
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+
+export const authOptions : NextAuthOptions = {
+    providers:[
+        CreadentialsProvider({
+           id: "credentials",
+           name: "Credentials",
+           credentials:{
+            email:{
+                label: "Username", type: "text"
+            },
+            password: {label: "Password", type: "password"}
+           },
+
+           async authorize(credentials:any): Promise<any>{
+
+            await dbConnect()
+            try{
+               const user = await UserModel.findOne({
+
+                    $or:[
+                        {email:credentials.identifier},
+                        {username: credentials.identifier}
+                    ]
+                })
+                if(!user){
+                    throw new Error('No user found with this email')
+                } 
+                if(!user.isVerified){
+                    throw new Error("Please verify first before login")
+                }
+
+              const isPasswordCorrect =  await bcrypt.compare(credentials.password, user.password)
+                if(isPasswordCorrect){
+                    return user
+                }else{
+                    throw new Error("Password is not correct")
+                }
+
+            } catch(err: any){
+                throw new Error(err)
+            }
+
+           }
+        })
+    ],
+    callbacks:{
+        async session({session, token}){
+            return session
+        },
+
+        async jwt({token, user}){
+            if(user){
+                token._id = user._id?. toString()
+            }
+            return token
+        },
+
+
+    },
+
+    pages:{
+        signIn: '/sign-in'
+    },
+    session: {
+        strategy: "jwt"
+    },
+    secret: process.env.AUTH_SECRET,
+}
