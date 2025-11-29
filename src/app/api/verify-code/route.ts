8eb6f9a25cbeaaa46b9d9638e333c1a0ef344aa1
vikrant dubey <Vikrant2003aug06@gmail.com>
2025-11-29 17:02:58 +0530

    Update route.ts

--- src/app/api/verify-code/route.ts
+++ src/app/api/verify-code/route.ts
@@ -1,8 +1,6 @@
 import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/model/User";
-// import { is } from "zod/v4/locales";
-// import {success, z} from 'zod'
-// import { usernameValidation} from "@/schemas/signUpSchema";
+
 
 export async function POST(request: Request){
     await dbConnect()
