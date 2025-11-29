1e4c1912dbe7b24944ec3d26bdb7e5bba75f11fe
vikrant dubey <Vikrant2003aug06@gmail.com>
2025-11-29 15:28:56 +0530

    updated

--- src/app/api/delete-message/[messageid]/route.ts
+++ src/app/api/delete-message/[messageid]/route.ts
@@ -9,9 +9,9 @@ import { authOptions } from "../../auth/[...nextauth]/options"
 // import { success } from "zod/v4";
 
 export async function DELETE(request: NextRequest,
-    context:  {params:{messageid:string}}
+    context:  {params:Promise<{ messageid: string }>}
 ){
-     const messageId = context.params.messageid;
+     const messageId = await context.params
      await dbConnect();
      const session = await getServerSession(authOptions)
      const _user: User = session?.user;
