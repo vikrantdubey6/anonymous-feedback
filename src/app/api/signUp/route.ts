356ff5e5571690a28d19aea183d3102c05f9b5c7
vikrant dubey <Vikrant2003aug06@gmail.com>
2025-11-29 15:56:16 +0530

    Update route.ts

--- src/app/api/signUp/route.ts
+++ src/app/api/signUp/route.ts
@@ -2,7 +2,7 @@ import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/model/User";
 import bcrypt from "bcryptjs";
 import { sendVerificationEmail } from "@/helpers/sendVerficationEmail";
-import { success } from "zod";
+// import { success } from "zod";
 
 export async function POST(request: Request) {
   await dbConnect();
